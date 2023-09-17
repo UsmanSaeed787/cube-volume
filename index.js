925448db7df99b9bbf33d308b739d24399e8faf0
UsmanSaeed787 <usmanalisaeed787@gmail.com>
2023-09-17 20:58:15 +0500

    Volume of Cube

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+//Volume Of Cube
+var length = 10;
+var width = 20;
+var height = 30;
+var volume = length ** 3;
+console.log(volume);
+var volume2 = length * height * width;
+console.log(volume2);
---
+++ index.ts
@@ -0,0 +1,11 @@
+//Volume Of Cube
+
+var length: number = 10;
+var width: number = 20;
+var height: number = 30;
+
+var volume: number = length ** 3;
+
+console.log(volume);
+var volume2: number = length * height * width
+console.log(volume2);
